a8dc2c1910c5d6962107d6ea3afc096d76f34020
Welle77 <simon.wessmann.brok@gmail.com>
2021-10-21 12:41:00 +0200

    Clean up this mess of an init

--- trifork/trifork/src/Pages/Albums/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Typography } from "@mui/material";
-import { Props } from "./interfaces";
-
-const Albums = (props: Props) => {
-  return <Typography>Albums</Typography>;
-};
-
-export default Albums;
